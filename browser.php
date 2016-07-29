efe69680560463ff7cae5d4e1d129ff5919c2a2e
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-29 00:35:00 -0500

    Added browser

---
+++ browser.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Comments browser
+ *
+ * @package    BardCanvas
+ * @subpackage comments
+ * @author     Alejandro Caballero - lava.caballero@gmail.com
+ *
+ * @var template $template
+ * @var account  $account
+ */
+
+use hng2_base\account;
+use hng2_base\template;
+
+include "../config.php";
+include "../includes/bootstrap.inc";
+if( ! $account->_exists ) throw_fake_404();
+
+$template->page_contents_include = "contents/browser.inc";
+$template->set_page_title($current_module->language->index->title);
+include "{$template->abspath}/embeddable.php";
